370814af3b68bf1b5c46e8cc5a04ac574556ac1c
Douelfiqar <douidriss@gmail.com>
2023-12-01 13:17:53 +0100

    first commit

---
+++ src/main/java/com/example/simulationducontroleaerien/SimulationDuControleAerienApplication.java
@@ -0,0 +1,13 @@
+package com.example.simulationducontroleaerien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulationDuControleAerienApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimulationDuControleAerienApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/simulationducontroleaerien/SimulationDuControleAerienApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simulationducontroleaerien;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimulationDuControleAerienApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
