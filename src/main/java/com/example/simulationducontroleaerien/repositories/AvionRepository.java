63e2949be12f71a88b2178bec02b152c1e37353f
Douelfiqar <douidriss@gmail.com>
2023-12-31 12:57:41 +0100

    edit dtos

--- src/main/java/com/example/simulationducontroleaerien/repositories/AvionRepository.java
+++ src/main/java/com/example/simulationducontroleaerien/repositories/AvionRepository.java
@@ -4,8 +4,11 @@ import com.example.simulationducontroleaerien.entities.Avion;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AvionRepository extends JpaRepository<Avion, Integer> {
     public Avion findAvionByNumeroSerie(String numeroSerie);
     public void deleteAvionByNumeroSerie(String numeroSerie);
+    public List<Avion> findAvionByTypeAvion(String typeAvion);
 }
--- src/main/java/com/example/simulationducontroleaerien/repositories/VolRepository.java
+++ src/main/java/com/example/simulationducontroleaerien/repositories/VolRepository.java
@@ -1,9 +1,13 @@
 package com.example.simulationducontroleaerien.repositories;
 
+import com.example.simulationducontroleaerien.entities.Avion;
 import com.example.simulationducontroleaerien.entities.Vol;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface VolRepository extends JpaRepository<Vol, Integer> {
+
 }
